0316fb5214c72f1c16bbbedbfb017ce868b0f183
Ansh <ansh20snew@gmail.com>
2026-02-19 14:43:15 +0530

    promise my

---
+++ promise.js
@@ -0,0 +1,13 @@
+const myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        const success = Math.random() > 0.5;
+        if(success){
+            resolve('operation completed successfully');
+        }else{
+            reject(new Error ('Operation failed'));
+        }
+    }, 1000);
+});
+myPromise
+    .then(result => console.log('Success:', result))
+    .catch(error => console.error('error:', error.message));
